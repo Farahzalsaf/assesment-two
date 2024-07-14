86717e85e13c3d747dec434baac6395751844f34
Farah <farahzalsafwan@gmail.com>
2024-07-14 20:17:39 +0300

    fixed

--- wb/tests/test_webserver.py
+++ wb/tests/test_webserver.py
@@ -1,9 +1,5 @@
-import sys
-import os
 import unittest
 
-# Add the project directory to the system path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from webserver import MyServer
 
